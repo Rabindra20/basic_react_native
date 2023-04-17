e4a6f7d8a4b7e792059c4fc7b4e7852522eee3ab
rabindra20 <rabindra2079@gmail.com>
2023-04-17 17:55:39 +0545

    updated

--- App.tsx
+++ App.tsx
@@ -1,104 +1,49 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
   useColorScheme,
   View,
+  Button,
 } from 'react-native';
 
-import {
-  Colors,
-  // Header,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import Bio_data from './component/bio_data';
+var lastname="Sah"
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const midlename= "Raj"
+  
+  // const backgroundStyle = {
+  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  // };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        {/* <Header /> */}
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="TEST">
-            <Text style={styles.highlight}>Hello rabindra</Text> 
-          </Section>
+          <Text style={{fontSize: 30}}>Hi Rabindra {midlename} {lastname}</Text>
+          <Text style={{fontSize: 20}}>{lastname==="Sah"?"Above info is correct":"Above info is wrong"}</Text>
+          <TEST />
+          <Bio_data />
+          <Button title='press here' onPress={Onpres}></Button>
+          {/* pass parameter in button */}
+          <Button title='Click here' onPress={()=>Onpress("hello")} color={"green"}></Button> 
         </View>
-      </ScrollView>
-    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
+const TEST=()=>{
+return(
+  <View>
+  <Text style={{fontSize: 20}}>For test another component</Text>
+  </View>
+)
+};
+const Onpres=()=>{
+  console.warn("Button working Fine")
+  };
+  {/* For pass parameter in button */}
+  const Onpress=(val:string)=>{
+    console.warn(val)
+    };
 export default App;
---
+++ component/bio_data.tsx
@@ -0,0 +1,12 @@
+import {
+    Text,
+    View,
+  } from 'react-native';
+  const Bio_data=()=>{
+    return (
+        <View>
+          <Text style={{fontSize: 20}}>Test from folderwise component</Text>
+        </View>
+  );
+}
+export default Bio_data
